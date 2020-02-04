9df3b7254d412b9d09026dbbcfae4103d0142310
Osman Yavuz <58117722+OsmanYavuz-web@users.noreply.github.com>
2020-02-05 00:34:41 +0300

    Unity 3D Sürükle Bırak
    Unity 3D, nesneleri sürükle bırak örneği.
    
    Ekleyeceğiniz gameobject nesnelerine Box Collider eklemeniz gerekmektedir.

---
+++ Assets/SurukleBirak.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurukleBirak : MonoBehaviour
+{
+
+    Vector3 objePozisyon;
+
+
+    private void OnMouseDown()
+    {
+        objePozisyon = Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 poz = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objePozisyon.z);
+        transform.position = Camera.main.ScreenToWorldPoint(poz);
+    }
+
+
+}
